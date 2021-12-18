1bfa97fa7bab832811b3a229d3e1c0bf4102ab47
Andrew Belt <andrewpbelt@gmail.com>
2021-12-17 20:08:33 -0500

    Add osdialog_prompt() implementation on Windows.

--- osdialog_win.c
+++ osdialog_win.c
@@ -60,17 +60,170 @@ int osdialog_message(osdialog_message_level level, osdialog_message_buttons butt
 }
 
 
+/*
+Helpful resources:
+https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-dlgtemplate
+https://stackoverflow.com/a/2270250/272642
+*/
+#define DLG_FONT L"MS Shell Dlg"
+#define DLG_OK L"&OK"
+#define DLG_CANCEL L"&Cancel"
+#define LENGTHOF(a) (sizeof(a) / sizeof((a)[0]))
+
+#pragma pack(push, 4)
+static struct {
+	DWORD style;
+	DWORD dwExtendedStyle;
+	WORD  cdit; // number of controls
+	short x;
+	short y;
+	short cx;
+	short cy;
+	WORD menu;
+	WORD windowClass;
+	WCHAR title[1];
+	short pointSize;
+	WCHAR typeface[LENGTHOF(DLG_FONT)];
+
+	struct {
+		DWORD style;
+		DWORD dwExtendedStyle;
+		short x;
+		short y;
+		short cx;
+		short cy;
+		WORD  id;
+		WORD sysClass; // 0xFFFF identifies a system window class
+		WORD idClass; // ordinal of a system window class
+		WCHAR title[1];
+		WORD cbCreationData; // bytes of following creation data
+	} edit;
+
+	struct {
+		DWORD style;
+		DWORD dwExtendedStyle;
+		short x;
+		short y;
+		short cx;
+		short cy;
+		WORD  id;
+		WORD sysClass; // 0xFFFF identifies a system window class
+		WORD idClass; // ordinal of a system window class
+		WCHAR title[LENGTHOF(DLG_OK)];
+		WORD cbCreationData; // bytes of following creation data
+	} ok;
+
+	struct {
+		DWORD style;
+		DWORD dwExtendedStyle;
+		short x;
+		short y;
+		short cx;
+		short cy;
+		WORD  id;
+		WORD sysClass; // 0xFFFF identifies a system window class
+		WORD idClass; // ordinal of a system window class
+		WCHAR title[LENGTHOF(DLG_CANCEL)];
+		WORD cbCreationData; // bytes of following creation data
+	} cancel;
+
+} promptTemplate = {
+	WS_POPUP | WS_VISIBLE | WS_CAPTION | WS_SYSMENU | DS_MODALFRAME | DS_CENTER | DS_SHELLFONT,
+	0x0, // dwExtendedStyle
+	3, // cdit
+	0, 0, 5+200+10+50+5+50+5, 5+14+5,
+	0, // menu
+	0, // windowClass
+	L"", // title
+	8, // typeface
+	DLG_FONT,
+
+	{
+		WS_CHILD | WS_VISIBLE | WS_TABSTOP | WS_BORDER | ES_LEFT | ES_AUTOHSCROLL,
+		WS_EX_NOPARENTNOTIFY,
+		5, 5, 200, 14,
+		42,
+		0xFFFF, 0x0081, // Edit
+		L"", 0,
+	},
+
+	{
+		WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_DEFPUSHBUTTON,
+		WS_EX_NOPARENTNOTIFY,
+		5+200+10, 5, 50, 14,
+		IDOK,
+		0xFFFF, 0x0080, // Button
+		DLG_OK, 0,
+	},
+
+	{
+		WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_PUSHBUTTON,
+		WS_EX_NOPARENTNOTIFY,
+		5+200+10+50+5, 5, 50, 14,
+		IDCANCEL,
+		0xFFFF, 0x0080, // Button
+		DLG_CANCEL, 0,
+	},
+};
+#pragma pack(pop)
+
+static wchar_t promptBuffer[1 << 13] = L"";
+
+static INT_PTR CALLBACK promptProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+	(void) lParam;
+
+	switch (message) {
+		case WM_INITDIALOG: {
+			SendDlgItemMessageW(hDlg, 42, WM_SETTEXT, (WPARAM) 0, (LPARAM) promptBuffer);
+			return TRUE;
+		} break;
+
+		case WM_DESTROY: {
+			EndDialog(hDlg, 0);
+			return TRUE;
+		} break;
+
+		case WM_COMMAND: {
+			switch (wParam) {
+				case IDOK: {
+					int len = SendDlgItemMessageW(hDlg, 42, WM_GETTEXT, (WPARAM) LENGTHOF(promptBuffer), (LPARAM) promptBuffer);
+					(void) len;
+					EndDialog(hDlg, 1);
+					return TRUE;
+				} break;
+
+				case IDCANCEL: {
+					EndDialog(hDlg, 0);
+					return TRUE;
+				} break;
+			}
+		} break;
+	}
+	return FALSE;
+}
+
 char* osdialog_prompt(osdialog_message_level level, const char* message, const char* text) {
-	// TODO
 	(void) level;
 	(void) message;
-	(void) text;
+
+	promptBuffer[0] = 0;
+	if (text) {
+		MultiByteToWideChar(CP_UTF8, 0, text, -1, promptBuffer, LENGTHOF(promptBuffer));
+	}
+
+	HWND window = GetActiveWindow();
+	int res = DialogBoxIndirectParamW(NULL, (LPCDLGTEMPLATEW) &promptTemplate, window, promptProc, (LPARAM) NULL);
+	if (res) {
+		return wchar_to_utf8(promptBuffer);
+	}
 	return NULL;
 }
 
 
-static INT CALLBACK browseCallbackProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
-	if (Msg == BFFM_INITIALIZED) {
+static INT CALLBACK browseCallbackProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	(void) wParam;
+
+	if (message == BFFM_INITIALIZED) {
 		SendMessageW(hWnd, BFFM_SETEXPANDED, 1, lParam);
 	}
 	return 0;
@@ -91,7 +244,7 @@ char* osdialog_file(osdialog_file_action action, const char* path, const char* f
 			wchar_t* pathW = utf8_to_wchar(path);
 			GetFullPathNameW(pathW, MAX_PATH, initialDir, NULL);
 			OSDIALOG_FREE(pathW);
-			bInfo.lpfn = browseCallbackProc;
+			bInfo.lpfn = (BFFCALLBACK) browseCallbackProc;
 			bInfo.lParam = (LPARAM) initialDir;
 		}
 
